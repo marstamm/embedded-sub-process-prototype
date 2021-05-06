952eb35c300fb25c54a57ebf649e13e4603366eb
Martin Stamm <martin.stamm@camunda.com>
2021-05-06 13:57:34 +0200

    add tree view

---
+++ buttons/concepts/navigation/tree.js
@@ -0,0 +1,57 @@
+import { fire, on } from "../eventBus.js";
+import { applyCss, createElement } from "../util.js";
+
+let active = false;
+
+const createTree = (level) => {
+  const template = level < 10 ? `<ul class="nested">
+  <li class='single'>Empty</li>
+  <li class='expand'><span class="caret"></span><span class="open">Level ${level}</span>
+</ul>` : `<li class='single'></li>`
+
+  let child = createElement(template);
+
+  const single = child.querySelector('.single')
+  single?.addEventListener('click', () => {
+    fire('open', level, false);
+  });
+
+  let expand = child.querySelector('.expand')
+  if(expand) {
+    expand.querySelector('.caret').addEventListener("click", () => {
+      expand.querySelector('.nested').classList.toggle("active");
+    });
+
+    expand.querySelector('.open').addEventListener("click", () => {
+      fire('open', level);
+    });
+
+    expand.appendChild(createTree(level + 1));
+  }
+
+  return child;
+}
+
+let element = createTree(0);
+console.log(element);
+
+element.classList.add('active');
+const style = {
+  display: 'none',
+  position: 'absolute',
+  top: '150px',
+  color: 'blue',
+  backgroundColor: 'rgba(255, 255, 255, 0.5)',
+  cursor: 'pointer',
+  left: '60px',
+  border: '1px solid black',
+  paddingLeft: '30px'
+
+}
+applyCss(element, style);
+
+on('navigation-change', type => {
+  active = type === 'tree';
+  element.style.display = active ? "" : "none";
+})
+document.body.appendChild(element);
