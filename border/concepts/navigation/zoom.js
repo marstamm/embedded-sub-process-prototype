fd210456c33a8c649989fe1cda020ef1b3559bdd
Martin Stamm <martin.stamm@camunda.com>
2021-05-19 15:42:44 +0200

    add sequenz flows

--- border/concepts/navigation/zoom.js
+++ border/concepts/navigation/zoom.js
@@ -1,10 +1,51 @@
 import { fire, on } from "../eventBus.js";
 import processMap from "../processMap.js";
-import { applyCss } from "../util.js";
+import { applyCss, fetchDiagram, createElement } from "../util.js";
+
+let arrows;
+fetchDiagram('resources/sequenceFlow.svg').then(res => {
+  arrows = [
+    createElement(`<div>${res}</div>`),
+    createElement(`<div>${res}</div>`)
+  ];
+
+  const baseStyle = {
+    // display: 'inline',
+    position: 'absolute',
+    transform: 'scale(1.5)',
+    zIndex: '-100',
+    display: 'none'
+  }
+
+  applyCss(arrows[0], {
+    left: '-160px',
+    top: '375px',
+    ...baseStyle,
+  });
+
+  applyCss(arrows[1], {
+    right: '490px',
+    top: '375px',
+    ...baseStyle,
+  });
+
+  modeler.parentElement.insertBefore(arrows[0], modeler);
+  modeler.after(arrows[1]);
+  // modeler.prepend(arrows[0]);
+  // modeler.appendChild(arrows[1]);
+});
 
 
 on('open', level => {
   if(level != 0) {
+    applyCss(arrows[0], {
+      display: 'inline'
+    });
+
+    applyCss(arrows[1], {
+      display: 'inline'
+    });
+
     applyCss(diagram_container, {
       border: '5px solid black',
       borderRadius: '50px',
@@ -17,13 +58,24 @@ on('open', level => {
     button.innerText = "-";
     button.contentEditable = false;
 
-    // div.querySelector('text tspan').innerHTML = processMap[level]?.name;
-    // console.log(div.querySelector('text tspan'));
     button.addEventListener('click', () => {
       fire('open', processMap[level]?.parent);
     });
 
-    // div.appendChild(button);
+    const title = document.createElement('dic');
+    title.innerText = processMap[level]?.name;
+    title.contentEditable = true;
+
+    applyCss(title, {
+      position: 'absolute',
+      top: '15px',
+      left: '0',
+      fontSize: '20',
+      width: '100%',
+      verticalAlign: 'middle',
+      textAlign: 'center'
+    });
+
     applyCss(button, {
       position: 'absolute',
       bottom: '5px',
@@ -38,8 +90,17 @@ on('open', level => {
     });
 
     diagram_container.appendChild(button);
+    diagram_container.appendChild(title);
     return;
   }
+
+  applyCss(arrows[0], {
+    display: 'none'
+  })
+
+  applyCss(arrows[1], {
+    display: 'none'
+  })
 
   applyCss(diagram_container, {
     border: '',
